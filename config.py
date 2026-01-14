a4927988a530f81b83e672186a638e62f80d0d7d
Saagar Jhawar <saagarjhawar84@gmail.com>
2026-01-14 08:37:05 +0530

    commit all

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ APP_TITLE = "💖 Aarushi's Smart Workout Coach"
 GOAL = "Build strong, visible abs with home workouts"
 
 CORE_EXERCISES = [
-    "Plank",
+    "Side Plank",
     "Crunches",
     "Leg Raises",
     "Mountain Climbers",
@@ -12,15 +12,24 @@ CORE_EXERCISES = [
     "Flutter Kicks",
     "knee ins"
     "plank dips"
+    "toe touches"
+    "toe taps"
+    "scissor jumps"
+    
     
 ]
 
 FULL_BODY_EXERCISES = [
-    "Squats",
+    "Squat Jumps",
     "Push-ups",
     "Lunges",
     "Jumping Jacks",
     "Burpees",
     "High Knees",
     "Yoga Flow"
+    "Cardio"
+    "Jumping Rope"
+    "High Knees"
+    "Burpees"
+    
 ]
